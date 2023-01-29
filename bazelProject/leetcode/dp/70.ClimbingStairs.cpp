5c362556e571bcf4b1737e5b22156489084a567d
sharwen <sharwen@sharwens-MBP.lan>
2023-01-29 15:18:17 +0800

    --70

---
+++ bazelProject/leetcode/dp/70.ClimbingStairs.cpp
@@ -0,0 +1,21 @@
+#include "70.ClimbingStairs.h"
+#include <vector>
+
+int climbStairs(int n)
+{
+    std::vector<int> dp(3, 0);
+    dp[1] = 1;
+    dp[2] = 2;
+    // dp[i] = dp[i-1] + dp[i-2]
+    if(n == 1 || n ==2) {
+        return dp[n];
+    }
+
+    for(int i = 2; i <n; ++i) 
+    {
+        dp[0] = dp[1] + dp[2];
+        dp[1] = dp[2];
+        dp[2] = dp[0];
+    }
+    return dp[0];
+}
---
+++ bazelProject/leetcode/dp/70.ClimbingStairsTest.cpp
@@ -0,0 +1,32 @@
+#include "70.ClimbingStairs.h"
+#include "gtest/gtest.h"
+#include <vector>
+
+
+TEST(case_1,content)
+{
+    int n = 1;
+    int ans = 1;
+    EXPECT_EQ(climbStairs(n), ans);
+}
+
+TEST(case_2,content)
+{
+    int n = 2;
+    int ans = 2;
+    EXPECT_EQ(climbStairs(n), ans);
+}
+
+TEST(case_3,content)
+{
+    int n = 3;
+    int ans = 3;
+    EXPECT_EQ(climbStairs(n), ans);
+}
+
+TEST(case_4,content)
+{
+    int n = 4;
+    int ans = 5;
+    EXPECT_EQ(climbStairs(n), ans);
+}

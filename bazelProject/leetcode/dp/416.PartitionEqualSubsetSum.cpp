4e607bd7fd5766d10276c436d5dcf68850a86d27
sharwen <sharwen@sharwens-MBP.lan>
2023-02-01 22:05:36 +0800

    --416

---
+++ bazelProject/leetcode/dp/416.PartitionEqualSubsetSum.cpp
@@ -0,0 +1,9 @@
+#include "416.PartitionEqualSubsetSum.h"
+
+// 0/1背包 和完全背包
+// https://blog.csdn.net/m0_46201544/article/details/123276427
+
+bool canPartition(vector<int>& nums)
+{
+    
+}
---
+++ bazelProject/leetcode/dp/416.PartitionEqualSubsetSum.h
@@ -0,0 +1,4 @@
+#include <vector>
+using namespace std;
+
+bool canPartition(vector<int>& nums);

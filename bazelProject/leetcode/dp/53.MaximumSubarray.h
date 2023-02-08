68a410c674f6d94c9f8b01d0544573ecba707fb6
sharwen <sharwen@sharwens-MBP.lan>
2023-02-09 00:20:28 +0800

    --5

---
+++ bazelProject/leetcode/dp/53.MaximumSubarray.h
@@ -0,0 +1,5 @@
+#include <vector>
+using namespace std;
+
+int maxSubArray(vector<int>& nums);
+int maxSubArray1(vector<int>& nums);
---
+++ bazelProject/leetcode/dp/53.MaximumSubarrayTest.cpp
@@ -0,0 +1,36 @@
+#include "53.MaximumSubarray.h"
+#include "gtest/gtest.h"
+#include <vector>
+
+
+TEST(case_1,content)
+{
+    std::vector<int> nums = {-2,1,-3,4,-1,2,1,-5,4};
+    int ans = 6;
+    EXPECT_EQ(maxSubArray(nums), ans);
+    EXPECT_EQ(maxSubArray1(nums), ans);
+}
+
+TEST(case_2,content)
+{
+    std::vector<int> nums = {1};
+    int ans = 1;
+    EXPECT_EQ(maxSubArray(nums), ans);
+    EXPECT_EQ(maxSubArray1(nums), ans);
+}
+
+TEST(case_3,content)
+{
+    std::vector<int> nums = {5,4,-1,7,8};
+    int ans = 23;
+    EXPECT_EQ(maxSubArray(nums), ans);
+    EXPECT_EQ(maxSubArray1(nums), ans);
+}
+
+TEST(case_4,content)
+{
+    std::vector<int> nums = {0};
+    int ans = 0;
+    EXPECT_EQ(maxSubArray(nums), ans);
+    EXPECT_EQ(maxSubArray1(nums), ans);
+}

f2fd686f9335e00201d010d433292f2e6f8dba63
sharwen <sharwen@sharwendeMacBook-Pro.local>
2023-02-22 20:31:50 +0800

    --494

--- bazelProject/leetcode/dp/494.TargetSum.h
+++ bazelProject/leetcode/dp/494.TargetSum.h
@@ -2,3 +2,6 @@
 using namespace std;
 
 int findTargetSumWays(vector<int>& nums, int target);
+int findTargetSumWays1(vector<int>& nums, int target);
+int findTargetSumWays3(vector<int>& nums, int target);
+int findTargetSumWays4(vector<int>& nums, int target);
--- bazelProject/leetcode/dp/494.TargetSumTest.cpp
+++ bazelProject/leetcode/dp/494.TargetSumTest.cpp
@@ -8,4 +8,17 @@ TEST(case_1,content)
     std::vector<int> nums = {1,1,1,1,1};
     int ans = 5, target = 3;
     EXPECT_EQ(findTargetSumWays(nums, target), ans);
+    EXPECT_EQ(findTargetSumWays1(nums, target), ans);
+    EXPECT_EQ(findTargetSumWays3(nums, target), ans);
+    EXPECT_EQ(findTargetSumWays4(nums, target), ans);
+}
+
+TEST(case_2,content)
+{
+    std::vector<int> nums = {1000};
+    int ans = 1, target = -1000;
+    EXPECT_EQ(findTargetSumWays(nums, target), ans);
+    EXPECT_EQ(findTargetSumWays1(nums, target), ans);
+    EXPECT_EQ(findTargetSumWays3(nums, target), ans);
+    EXPECT_EQ(findTargetSumWays4(nums, target), ans);
 }

747bc94ee3f25626731ad50b13781c96ba3a8cf6
sharwen <sharwen@sharwendeMBP.lan>
2023-03-15 01:05:36 +0800

    --227

--- bazelProject/leetcode/string/227.BasicCalculatorII.cpp
+++ bazelProject/leetcode/string/227.BasicCalculatorII.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+
 inline bool isOpsub(const char & ch) {
     return ch =='+' || ch == '-';
 }
@@ -20,8 +21,7 @@ int nextNum(const string & s, int & idx) {
             continue;
         }
 
-        if(isOpsub(s[idx] || isOpPro(s[idx]))) {
-            --idx;
+        if(isOpsub(s[idx]) || isOpPro(s[idx])) {
             break;
         }
 
@@ -31,49 +31,49 @@ int nextNum(const string & s, int & idx) {
 }
 
 int calculate(string s) {
-    int tmp = 0, res, n1, n2;
-    stack<int> nums;
-    stack<char>op;
+    int64_t tmp = 0, res, n1, n2;
+    deque<int64_t> nums;
+    deque<char>op;
     for(int i = 0; i <= s.size(); ++i) {
         if(i == s.size()) {
-            nums.push(tmp);
+            nums.push_back(tmp);
             break;
         }
         if(s[i] == ' ') {
             continue;
         }
         if(isOpsub(s[i])) {
-            op.push(s[i]);
-            nums.push(tmp);
+            op.push_back(s[i]);
+            nums.push_back(tmp);
             tmp=0;
         }else if(isOpPro(s[i])) {
             char tmpch = s[i];
             n1 = tmp;
             ++i;
             n2 = nextNum(s, i);
+            --i;
             if(tmpch == '/') {
                 res = n1 / n2;
             }else {
                 res = n1 * n2;
             }
-            nums.push(res);
-            tmp=0;
+            tmp=res;
         }else {
             tmp = tmp * 10 + s[i] - '0';
         }
     }
 
     while(!op.empty()) {
-        char ch = op.top(); op.pop();
-        n2 = nums.top();nums.pop();
-        n1 = nums.top();nums.pop();
+        char ch = op.front(); op.pop_front();
+        n1 = nums.front();nums.pop_front();
+        n2 = nums.front();nums.pop_front();
         if(ch == '+') {
             res = n1 + n2;
         }else {
-            res = n1 - n1;
+            res = n1 - n2;
         }
-        nums.push(res);
+        nums.push_front(res);
     }
 
-    return nums.top();
+    return nums.front();
 }

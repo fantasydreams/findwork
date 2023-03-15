adca68259d6b689b6e6ae6be439c0052e569bbd2
sharwen <sharwen@sharwens-MacBook-Pro.local>
2023-03-16 00:28:46 +0800

    --28

---
+++ bazelProject/leetcode/string/28.FindtheIndexoftheFirstOccurrenceinaString.cpp
@@ -0,0 +1,40 @@
+#include "28.FindtheIndexoftheFirstOccurrenceinaString.h"
+#include <vector>
+
+void getNextArr(const std::string & s, vector<int> & vecNext) {
+    if(s.empty()) {
+        return;
+    }
+    vecNext.reserve(s.size());
+    vecNext[0] = -1;
+    int i = 0, j = -1;
+    while(i < s.size() - 1) {
+        if(j == -1 || s[i] == s[j]) {
+            ++i;
+            ++j;
+            vecNext[i] = j;
+        }else {
+            j = vecNext[j];
+        }
+    }
+}
+
+int strStr(string haystack, string needle) {
+    vector<int> vecNext;
+    getNextArr(needle, vecNext);
+    int i = 0, j = 0;
+    while(i < haystack.size()) {
+        if(j == -1 || haystack[i] == needle[j]) {
+            if(j == needle.size() - 1) {
+                return i - j;
+            }
+
+            ++i;
+            ++j;
+        }else {
+            j = vecNext[j];
+        }
+    }
+    
+    return -1;
+}

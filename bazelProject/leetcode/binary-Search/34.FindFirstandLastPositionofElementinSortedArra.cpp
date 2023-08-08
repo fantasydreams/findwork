c7d1579ea7d148457b4f8bcefadc33922c601437
fantasydreams <sharwen568@gmail.com>
2023-08-08 23:35:19 +0800

    --34

--- bazelProject/leetcode/binary-Search/34.FindFirstandLastPositionofElementinSortedArra.cpp
+++ bazelProject/leetcode/binary-Search/34.FindFirstandLastPositionofElementinSortedArra.cpp
@@ -1,4 +1,6 @@
 #include "34.FindFirstandLastPositionofElementinSortedArra.h"
+#include <algorithm>
+#include <iterator>
 
 vector<int> searchRange(vector<int>& nums, int target) {
     int low = 0, high = nums.size() - 1, mid;
@@ -27,4 +29,39 @@ vector<int> searchRange(vector<int>& nums, int target) {
     }
 
     return {left, right};
+}
+
+
+
+// lower_bound( begin,end,num)：从数组的begin位置到end-1位置二分查找第一个大于或等于num的数字，找到返回该数字的地址，不存在则返回end。通过返回的地址减去起始地址begin,得到找到数字在数组中的下标。
+
+// upper_bound( begin,end,num)：从数组的begin位置到end-1位置二分查找第一个大于num的数字，找到返回该数字的地址，不存在则返回end。通过返回的地址减去起始地址begin,得到找到数字在数组中的下标。
+
+// 在从大到小的排序数组中，重载lower_bound()和upper_bound()
+
+// lower_bound( begin,end,num,greater<type>() ):从数组的begin位置到end-1位置二分查找第一个小于或等于num的数字，找到返回该数字的地址，不存在则返回end。通过返回的地址减去起始地址begin,得到找到数字在数组中的下标。
+
+// upper_bound( begin,end,num,greater<type>() ):从数组的begin位置到end-1位置二分查找第一个小于num的数字，找到返回该数字的地址，不存在则返回end。通过返回的地址减去起始地址begin,得到找到数字在数组中的下标。
+
+// auto pIterBegin = std::lower_bound(oDayDataList.item().begin(), oDayDataList.item().end(), oCursorRecord.pass_time(), 
+// 												[](const wwopenhwcheckin::AccessData& oData, uint32_t iTime){return oData.pass_time() > iTime;});
+// *mid值与value 比较，如果cmp条件成立则frist = mid + 1，掠过前面一半的数据
+			
+// 	auto pIterEnd = std::upper_bound(pIterBegin, oDayDataList.item().end(), oCursorRecord.pass_time(), 
+// 												[](uint32_t iTime, const wwopenhwcheckin::AccessData& oData){return iTime > oData.pass_time();});
+//  注意upper_bound是 value 与 *mid进行比较，条件成立则掠过后面一半的数据
+vector<int> searchRangeSTL(vector<int>& nums, int target) {
+    auto pIter = lower_bound(nums.begin(), nums.end(), target);
+    if(pIter == nums.end() || *pIter != target) {
+        return {-1, -1};
+    }
+
+    auto pIterEnd = upper_bound(pIter, nums.end(), target);
+    if(pIterEnd == nums.end()) {
+        return {(int)std::distance(nums.begin(), pIter), (int)nums.size()-1};
+    }else if(*pIterEnd != target) {
+        return {(int)std::distance(nums.begin(), pIter), (int)std::distance(nums.begin(), pIterEnd) - 1};
+    }else {
+        return {(int)std::distance(nums.begin(), pIter), (int)std::distance(nums.begin(), pIterEnd)};
+    }
 }
--- bazelProject/leetcode/binary-Search/34.FindFirstandLastPositionofElementinSortedArraTest.cpp
+++ bazelProject/leetcode/binary-Search/34.FindFirstandLastPositionofElementinSortedArraTest.cpp
@@ -8,6 +8,7 @@ TEST(case_1,content)
     int target = 2;
     std::vector<int> vec1 = {1,2,3}, ans = {1,1};
     EXPECT_EQ(searchRange(vec1, target), ans);
+    EXPECT_EQ(searchRangeSTL(vec1, target), ans);
 }
 
 
@@ -16,6 +17,7 @@ TEST(case_2,content)
     int target = 2;
     std::vector<int> vec1 = {2,2}, ans = {0,1};
     EXPECT_EQ(searchRange(vec1, target), ans);
+    EXPECT_EQ(searchRangeSTL(vec1, target), ans);
 }
 
 
@@ -24,4 +26,5 @@ TEST(case_3,content)
     int target = 2;
     std::vector<int> vec1 = {1,2,2,2,3,4}, ans = {1,3};
     EXPECT_EQ(searchRange(vec1, target), ans);
+    EXPECT_EQ(searchRangeSTL(vec1, target), ans);
 }

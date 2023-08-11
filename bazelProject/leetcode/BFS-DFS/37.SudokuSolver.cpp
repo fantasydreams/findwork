57065dffc84f766127ff0b486afd4bb27e0547e2
fantasydreams <sharwen568@gmail.com>
2023-08-12 00:47:42 +0800

    --37

--- bazelProject/leetcode/BFS-DFS/37.SudokuSolver.cpp
+++ bazelProject/leetcode/BFS-DFS/37.SudokuSolver.cpp
@@ -48,3 +48,47 @@ void solveSudoku(vector<vector<char>>& board)
 {
     solver(board);
 }
+
+
+bool validSudu(vector<vector<char> >& board, int r, int c, char ch) {
+    for(int i = 0; i < board.size(); ++i) {
+        if(board[r][i] == ch || board[i][c] == ch) {
+            return false;
+        }
+
+        if(board[(r / 3) * 3 + i / 3][(c / 3) * 3 + i % 3] == ch) { // 为什么呢？ :
+            return false;
+        }
+    }
+    return true;
+}
+
+
+int solver1(vector<vector<char> >& board) {
+    int n = board.size();
+    for(int r = 0; r < n; ++r) {
+        for(int c = 0; c < n; ++c) {
+            if(board[r][c] == '.') {
+                for(char ch = '1'; ch <= '9'; ++ch) {
+                    if(validSudu(board, r, c, ch)) {
+                        board[r][c] = ch;
+                        if(solver1(board)) {
+                            return true;
+                        }else {
+                            board[r][c] = '.';
+                        }
+                    }
+                }
+                return false;
+            }
+
+        }
+    }
+    return true;
+}
+
+
+void solveSudoku1(vector<vector<char> >& board)
+{
+    solver1(board);
+}
--- bazelProject/leetcode/BFS-DFS/37.SudokuSolver.h
+++ bazelProject/leetcode/BFS-DFS/37.SudokuSolver.h
@@ -1,3 +1,4 @@
 #include <vector>
 using namespace std;
 void solveSudoku(vector<vector<char>>& board);
+void solveSudoku1(vector<vector<char>>& board); // 2023/08/12

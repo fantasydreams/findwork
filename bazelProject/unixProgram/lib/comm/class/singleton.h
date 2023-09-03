f0b329a6b3f022079ea3844751d80324c86bb2ae
fantasydreams <sharwen568@gmail.com>
2023-09-03 18:13:50 +0800

    --singleton

--- bazelProject/unixProgram/lib/comm/class/singleton.h
+++ bazelProject/unixProgram/lib/comm/class/singleton.h
@@ -2,6 +2,7 @@
 #include <atomic>
 #include <utility>
 #include <mutex>
+#include <pthread.h>
 #include "no_copyable.h"
 
 namespace comm {
@@ -86,5 +87,169 @@ class SingletonStatic1 : public comm::NonCopyable {
             return &oInstace;
         }
 };
+
+
+// call_once once_flag
+template<class Type, typename ...Args>
+class SingletonCallOnce : comm::NonCopyable {
+    public:
+        static Type* GetInstance(Args&&... args) {
+            std::call_once(m_InitFlag, InitSingleton, std::forward<Args>(args)...);
+            return m_pInstace;
+        };
+
+        static void Destory() {
+            std::call_once(m_DestoryFlag, Release);
+        }
+    private:
+        static std::once_flag m_InitFlag;
+        static std::once_flag m_DestoryFlag;
+        static Type* m_pInstace;
+
+        static void InitSingleton(Args... args) {
+            m_pInstace = new Type(std::forward<Args>(args)...);
+        }
+        
+        static void Release() {
+            delete m_pInstace;
+            m_pInstace = nullptr;
+        }
+};
+
+template<class Type, typename...Args> std::once_flag SingletonCallOnce<Type, Args...>::m_InitFlag;
+template<class Type, typename...Args> std::once_flag SingletonCallOnce<Type, Args...>::m_DestoryFlag;
+template<class Type, typename...Args> Type* SingletonCallOnce<Type, Args...>::m_pInstace = nullptr;
+
+// operator new + placement new
+template <class Type, typename ...Args>
+class SingletonOperatorNew : comm::NonCopyable {
+    public:
+        static Type* GetInstance(Args&&... args) {
+            if(m_pInstace == nullptr) {
+                std::lock_guard<std::mutex> oLock(m_oMutex);
+                if(m_pInstace == nullptr) {
+                    Type* pTmp = static_cast<Type*>(operator new(sizeof(Type)));
+                    new(pTmp)Type(std::forward<Args>(args)...);
+                    m_pInstace = pTmp;
+                }
+            }
+            return m_pInstace;
+        };
+
+        static void Destory() {
+            std::lock_guard<std::mutex> oLock(m_oMutex);
+            delete m_pInstace;
+            m_pInstace = nullptr;
+        }
+    private:
+        static Type* m_pInstace;
+        static std::mutex m_oMutex;
+};
+
+template<class Type, typename...Args> Type* SingletonOperatorNew<Type, Args...>::m_pInstace = nullptr;
+template<class Type, typename...Args> std::mutex SingletonOperatorNew<Type, Args...>::m_oMutex;
+
+
+
+// barrier #define barrier() __asm__ volatile ("lwsync")
+// #define barrier() __asm__ volatile ("lwsync")
+
+// template<class Type, typename ...Args>
+// class SingletonBarrier : comm::NonCopyable {
+//     public:
+//         static Type* GetInstance(Args&&... args) {
+//             if(m_pInstace == nullptr) {
+//                 std::lock_guard<std::mutex> oLock(m_oMutex);
+//                 barrier();
+//                 if(m_pInstace == nullptr) {
+//                     m_pInstace = new Type(std::forward<Args>(args)...);
+//                 }
+//             }
+//             return m_pInstace;
+//         };
+
+//         static void Destory() {
+//             std::lock_guard<std::mutex> oLock(m_oMutex);
+//             delete m_pInstace;
+//             m_pInstace = nullptr;
+//         }
+//     private:
+//         static Type* m_pInstace;
+//         static std::mutex m_oMutex;
+// };
+
+// template<class Type, typename...Args> Type* SingletonBarrier<Type, Args...>::m_pInstace = nullptr;
+// template<class Type, typename...Args> std::mutex SingletonBarrier<Type, Args...>::m_oMutex;
+
+
+// 内存屏障
+// atomic_thread_fence memory_order_acquire memory_order_release 保证他们之间的语句不会乱序执行 
+
+template<class Type, typename ...Args>
+class SingletonMemFence : comm::NonCopyable {
+    public:
+        static Type* GetInstance(Args&&... args) {
+            Type * pInstace = m_pInstace.load(std::memory_order_relaxed);
+            std::atomic_thread_fence(std::memory_order_acquire);
+            if(pInstace == nullptr) {
+                std::lock_guard<std::mutex> oLock(m_oMutex);
+                pInstace = m_pInstace.load(std::memory_order_relaxed);
+                if(m_pInstace == nullptr) {
+                    pInstace = new Type(std::forward<Args>(args)...);
+                    m_pInstace.store(pInstace, std::memory_order_release);
+                    std::atomic_thread_fence(std::memory_order_release);
+                }
+            }
+            return pInstace;
+        };
+
+        static void Destory() {
+            std::lock_guard<std::mutex> oLock(m_oMutex);
+            std::atomic_thread_fence(std::memory_order_acquire);
+            Type* pInstace = m_pInstace.load(std::memory_order_relaxed);
+            delete m_pInstace;
+            m_pInstace.store(nullptr, std::memory_order_relaxed);
+            std::atomic_thread_fence(std::memory_order_release);
+        }
+    private:
+        static std::atomic<Type*> m_pInstace;
+        static std::mutex m_oMutex;
+};
+
+template <class Type, typename... Args> std::atomic<Type*> SingletonMemFence<Type, Args...>::m_pInstace;
+template <class Type, typename... Args> std::mutex SingletonMemFence<Type, Args...>::m_oMutex;
+
+
+
+// thread_once
+template<class Type>
+class SingletonPthreadOnce : comm::NonCopyable {
+    public:
+        static Type* GetInstance() {
+            pthread_once(&m_InitFlag, InitSingleton);
+            return m_pInstace;
+        };
+
+        static void Destory() {
+            pthread_once(&m_DestoryFlag, Destory);
+        }
+    private:
+        static pthread_once_t m_InitFlag;
+        static pthread_once_t m_DestoryFlag;
+        static Type* m_pInstace;
+
+        static void InitSingleton() {
+            m_pInstace = new Type();
+        }
+        
+        static void Release() {
+            delete m_pInstace;
+            m_pInstace = nullptr;
+        }
+};
+
+template<class Type> pthread_once_t SingletonPthreadOnce<Type>::m_InitFlag;
+template<class Type> pthread_once_t SingletonPthreadOnce<Type>::m_DestoryFlag;
+template<class Type> Type* SingletonPthreadOnce<Type>::m_pInstace = nullptr;
 
 }
--- bazelProject/unixProgram/lib/comm/class/test/singleton_test.cpp
+++ bazelProject/unixProgram/lib/comm/class/test/singleton_test.cpp
@@ -46,6 +46,48 @@ class D : public comm::SingletonStatic1<D> {
     private:
 };
 
+class E : public comm::SingletonCallOnce<E, int> {
+    public:
+    E(int numm) : m_num(numm){};
+    ~E() = default;
+    int Get() {
+        return m_num;
+    }
+
+    private:
+    int m_num;
+};
+
+class F : public comm::SingletonOperatorNew<F, int> {
+   public:
+    F(int numm) : m_num(numm){};
+    ~F() = default;
+    int Get() {
+        return m_num;
+    }
+
+    private:
+    int m_num; 
+};
+
+class G : public comm::SingletonMemFence<G, int> {
+    public:
+    G(int numm) : m_num(numm){};
+    ~G() = default;
+    int Get() {
+        return m_num;
+    }
+
+    private:
+    int m_num; 
+};
+
+class H : public comm::SingletonPthreadOnce<H> {
+    public:
+    H(){};
+    ~H() = default;
+};
+
 TEST(case_1, content)
 {
     auto pOject = A::GetInstance(123, 321);
@@ -87,5 +129,37 @@ TEST(case_4, content)
 {
     auto pOject = D::GetInstance();
     auto pOject1 = D::GetInstance();
+    EXPECT_EQ((uint64_t)pOject, (uint64_t)pOject1);
+}
+
+TEST(case_5, content)
+{
+    auto pOject = E::GetInstance(123);
+    auto pOject1 = E::GetInstance(321);
+    EXPECT_EQ(pOject1->Get(), 123);
+    EXPECT_EQ((uint64_t)pOject, (uint64_t)pOject1);
+}
+
+
+TEST(case_6, content)
+{
+    auto pOject = F::GetInstance(123);
+    auto pOject1 = F::GetInstance(321);
+    EXPECT_EQ(pOject1->Get(), 123);
+    EXPECT_EQ((uint64_t)pOject, (uint64_t)pOject1);
+}
+
+TEST(case_7, content)
+{
+    auto pOject = G::GetInstance(123);
+    auto pOject1 = G::GetInstance(321);
+    EXPECT_EQ(pOject1->Get(), 123);
+    EXPECT_EQ((uint64_t)pOject, (uint64_t)pOject1);
+}
+
+TEST(case_8, content)
+{
+    auto pOject = H::GetInstance();
+    auto pOject1 = H::GetInstance();
     EXPECT_EQ((uint64_t)pOject, (uint64_t)pOject1);
 }

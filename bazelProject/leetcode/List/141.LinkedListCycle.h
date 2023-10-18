ae86749be6f46f0135c44c744c2e9ed12152cee4
fantasydreams <Sharwen568@gmail.com>
2023-10-19 00:11:22 +0800

    --141

---
+++ bazelProject/leetcode/List/141.LinkedListCycle.h
@@ -0,0 +1,36 @@
+#include "linkcomm.h"
+#include <unordered_set>
+
+bool hasCycle(ListNode *head) {
+    if(head == nullptr) {
+        return false;
+    }
+
+    ListNode* pSlow = head, *pFast = head->next;
+    while(pSlow && pFast) {
+        if(pSlow == pFast) {
+            return true;
+        }
+        pSlow = pSlow->next;
+        pFast = pFast->next;
+        if(pFast) {
+            pFast = pFast->next;
+        }
+    }
+
+    return false;
+}
+
+bool hasCycle1(ListNode *head) {
+    if(!head) return false;
+    std::unordered_set<ListNode*> set;
+    while(head){
+        if(set.count(head)) {
+            return true;
+        }
+        set.insert(head);
+        head=head->next;
+    }
+    
+    return false;
+}

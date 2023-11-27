af21506b53fcb8cb82d0c226ea18f678cba97367
fantasydreams <Sharwen568@gmail.com>
2023-11-28 01:34:25 +0800

    --224

---
+++ bazelProject/leetcode/math/224.BasicCalculator.cpp
@@ -0,0 +1,101 @@
+#include "224.BasicCalculator.h"
+#include <queue>
+#include <stack>
+
+int64_t GetNextNumber(const std::string& str, int& idx) {
+    int64_t number = 0;
+    while(idx < str.size() && str[idx] == ' ') {
+        ++idx;
+    }
+
+    while(idx < str.size() && str[idx] >= '0' && str[idx] <= '9') {
+        number = number * 10 + str[idx] - '0';
+        ++idx;
+    }
+    // printf("1111; %d\n", idx);
+    return number;
+}
+
+
+int calculate(string s) {
+    std::deque<int64_t> stPara;
+    std::deque<char> stOp;
+
+    int idx = 0;
+
+    bool bOther = false;
+    while(idx < s.size()) {
+        if(s[idx] == ' ') {
+            ++idx;
+            continue;
+        }
+
+        if(s[idx] == '(' || s[idx] == ')' || s[idx] == '+' || s[idx] == '-') {
+            if(s[idx] == '(') {
+                bOther = true;
+                stOp.push_back('(');
+                ++idx;
+                stPara.push_back(GetNextNumber(s, idx));
+            }else if(s[idx] == ')') {
+                bOther = true;
+                ++idx;
+                char op = stOp.back(); stOp.pop_back();
+                stack<int64_t> stPara1; stPara1.push(stPara.back()); stPara.pop_back();
+                stack<char> stOP1;
+                while(op != '(') {
+                    stOP1.push(op);
+                    stPara1.push(stPara.back()); stPara.pop_back();
+                    op = stOp.back(); stOp.pop_back();
+                }
+
+                while(!stOP1.empty()) {
+                    op = stOP1.top(); stOP1.pop();
+                    int64_t a1 = stPara1.top(); stPara1.pop();
+                    int64_t a2 = stPara1.top(); stPara1.pop();
+                    if(op == '+') {
+                        stPara1.push(a1 + a2);
+                    }else {
+                        stPara1.push(a1 - a2);
+                    }
+                }
+
+                stPara.push_back(stPara1.top());
+            }else {
+                if(bOther == false) {
+                    stPara.push_back(0);
+                    bOther = true;
+                }
+                stOp.push_back(s[idx++]);
+                while(idx < s.size() && s[idx] == ' ') {
+                    ++idx;
+                    continue;
+                }
+                if(idx >= s.size()) {
+                    break;
+                }
+                if(s[idx] == '(' || s[idx] == ')') {
+                    bOther = false;
+                    continue;
+                }
+
+                stPara.push_back(GetNextNumber(s, idx));
+            }
+        }else {
+            bOther = true;
+            stPara.push_back(GetNextNumber(s, idx));
+        }
+    }
+
+    while(!stOp.empty()) {
+        int64_t a1 = stPara.front(); stPara.pop_front();
+        int64_t a2 = stPara.front(); stPara.pop_front();
+        char op = stOp.front();stOp.pop_front();
+        if(op == '+') {
+            stPara.push_front(a1 + a2);
+        }else {
+            stPara.push_front(a1 - a2);
+        }
+    }
+
+    return stPara.empty() ? 0 : stPara.front();
+}
---
+++ bazelProject/leetcode/math/224.BasicCalculatorTest.cpp
@@ -0,0 +1,78 @@
+#include "224.BasicCalculator.h"
+#include "gtest/gtest.h"
+
+TEST(BasicCalculator1, function1) {
+    std::string s = "1 + 1";
+    int ans = 2;
+
+    EXPECT_EQ(calculate(s), ans);
+}
+
+
+TEST(BasicCalculator2, function2) {
+    std::string s = " 2-1 + 2 ";
+    int ans = 3;
+
+    EXPECT_EQ(calculate(s), ans);
+}
+
+TEST(BasicCalculator3, function3) {
+    std::string s = "(1+(4+5+2)-3)+(6+8)";
+    int ans = 23;
+
+    EXPECT_EQ(calculate(s), ans);
+}
+
+TEST(BasicCalculator4, function3) {
+    std::string s = "";
+    int ans = 0;
+
+    EXPECT_EQ(calculate(s), ans);
+}
+
+
+
+TEST(BasicCalculator5, function3) {
+    std::string s = "  ";
+    int ans = 0;
+
+    EXPECT_EQ(calculate(s), ans);
+}
+
+
+TEST(BasicCalculator6, function3) {
+    std::string s = " (1-(4-5+2)-3)+(6-8) ";
+    int ans = -5;
+
+    EXPECT_EQ(calculate(s), ans);
+}
+
+
+TEST(BasicCalculator7, function3) {
+    std::string s = " -2+ 1 ";
+    int ans = -1;
+
+    EXPECT_EQ(calculate(s), ans);
+}
+
+TEST(BasicCalculator8, function3) {
+    std::string s = " +2";
+    int ans = 2;
+
+    EXPECT_EQ(calculate(s), ans);
+}
+
+TEST(BasicCalculator9, function3) {
+    std::string s = "-2+ (-1)";
+    int ans = -3;
+
+    EXPECT_EQ(calculate(s), ans);
+}
+
+
+TEST(BasicCalculator10, function3) {
+    std::string s = "- (3 + (4 + 5))";
+    int ans = -12;
+
+    EXPECT_EQ(calculate(s), ans);
+}
